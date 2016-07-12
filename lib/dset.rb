10695be17be4ec9bb75f129450c6ab175244f36e
David Ladowitz <david@ladowitz.com>
2016-07-12 11:42:24 -0700

    adding superset and specs

--- lib/dset.rb
+++ lib/dset.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class DSet
   def initialize(array = [])
     @hash = Hash.new(false)
@@ -12,7 +14,7 @@ class DSet
     @hash[element] = true
   end
 
-  def includes?(element)
+  def include?(element)
     @hash[element] ? true : false
   end
 
@@ -28,11 +30,17 @@ class DSet
     collection.each { |element| add(element) }
   end
 
-  def subset?(collection)
+  def has_subset?(collection)
     collection.each do |element|
-      unless includes? element
-        return false
-      end
+      return false unless include? element
+    end
+
+    return true
+  end
+
+  def in_superset?(collection)
+    self.each do |element|
+      return false unless collection.include? element
     end
 
     return true
--- spec/dset_spec.rb
+++ spec/dset_spec.rb
@@ -1,8 +1,8 @@
 require 'dset.rb'
+require 'pry'
 
 describe DSet do
   context "with no sets or elements" do
-
     it "creates a new set" do
       @dset = DSet.new
       expect(@dset).to be_an_instance_of DSet
@@ -12,97 +12,112 @@ describe DSet do
       @dset = DSet.new(["a", "b", "c"])
       expect(@dset.length).to eq 3
     end
+  end
 
-    context "with an empty set" do
-      before { @dset = DSet.new }
+  context "with an empty set" do
+    before { @dset = DSet.new }
 
-      describe '#length' do
-        it "returns the length of the set" do
-          expect(@dset.length).to eq 0
-        end
-      end
-
-      describe "#add" do
-        it "correcly adds elements to the set" do
-          @dset.add "a"
-          expect(@dset.length).to eq 1
-        end
-      end
-
-      describe "#includes?" do
-        it "returns true if an element is in the set" do
-          @dset.add "a"
-          expect(@dset.includes? "a").to be true
-        end
-
-        it "returns false if an element is not in the set" do
-          expect(@dset.includes? "z").to be false
-        end
-      end
-
-      describe "#delete" do
-        it "deletes an item if its in the set" do
-          @dset.add "a"
-          @dset.delete "a"
-          expect(@dset.includes? "a").to be false
-        end
-
-        it "does nothing if an item is not in the set" do
-          @dset.delete "a"
-          expect(@dset.includes? "a").to be false
-        end
-      end
-
-      context "with elements already in the set" do
-        before { @dset = DSet.new(["a", "b", "c"]) }
-
-        describe "#each" do
-          it "returns acts on each of the elements in the collection" do
-            array = []
-            @dset.each { |element| array << element }
-            expect(array).to match_array ["a", "b", "c"]
-          end
-        end
-
-        describe "#merge" do
-          it "merges in an array of elements" do
-            @dset.merge ["x", "y", "z"]
-            expect(@dset.length).to eq 6
-            expect(@dset.includes? "z").to be true
-          end
-
-          it "merges in another set" do
-            dset2 = DSet.new ["x", "y", "z"]
-            @dset.merge dset2
-            expect(@dset.length).to eq 6
-          end
-        end
-
-        describe "#subset?" do
-          it "retuns true when all elements of an array are present" do
-            expect(@dset.subset? ["a", "b"]).to be true
-          end
-
-          it "retuns true when all elements of a set are present" do
-            dset2 = Set.new ["a", "b"]
-            expect(@dset.subset? dset2).to be true
-          end
-
-          it "returns false when an element from the array is missing from the set" do
-            expect(@dset.subset? ["x", "b"]).to be false
-          end
-
-          it "retuns true when an element of a set is missing" do
-            dset2 = Set.new ["x", "b"]
-            expect(@dset.subset? dset2).to be false
-          end
-        end
+    describe '#length' do
+      it "returns the length of the set" do
+        expect(@dset.length).to eq 0
       end
     end
 
-    # it "adds elements to the set" do
-    #   @dset.add "A"
-    #   expect(@dset.length).to eq 1
-    # end
+    describe "#add" do
+      it "correcly adds elements to the set" do
+        @dset.add "a"
+        expect(@dset.length).to eq 1
+      end
+    end
+
+    describe "#include?" do
+      it "returns true if an element is in the set" do
+        @dset.add "a"
+        expect(@dset.include? "a").to be true
+      end
+
+      it "returns false if an element is not in the set" do
+        expect(@dset.include? "z").to be false
+      end
+    end
+
+    describe "#delete" do
+      it "deletes an item if its in the set" do
+        @dset.add "a"
+        @dset.delete "a"
+        expect(@dset.include? "a").to be false
+      end
+
+      it "does nothing if an item is not in the set" do
+        @dset.delete "a"
+        expect(@dset.include? "a").to be false
+      end
+    end
+
+    context "with elements already in the set" do
+      before { @dset = DSet.new(["a", "b", "c"]) }
+
+      describe "#each" do
+        it "returns acts on each of the elements in the collection" do
+          array = []
+          @dset.each { |element| array << element }
+          expect(array).to match_array ["a", "b", "c"]
+        end
+      end
+
+      describe "#merge" do
+        it "merges in an array of elements" do
+          @dset.merge ["x", "y", "z"]
+          expect(@dset.length).to eq 6
+          expect(@dset.include? "z").to be true
+        end
+
+        it "merges in another set" do
+          dset2 = DSet.new ["x", "y", "z"]
+          @dset.merge dset2
+          expect(@dset.length).to eq 6
+        end
+      end
+
+      describe "#has_subset?" do
+        it "retuns true when all elements of an array are present" do
+          expect(@dset.has_subset? ["a", "b"]).to be true
+        end
+
+        it "retuns true when all elements of a set are present" do
+          dset2 = DSet.new ["a", "b"]
+          expect(@dset.has_subset? dset2).to be true
+        end
+
+        it "returns false when an element from the array is missing from the set" do
+          expect(@dset.has_subset? ["x", "b"]).to be false
+        end
+
+        it "retuns true when an element of a set is missing" do
+          dset2 = DSet.new ["x", "b"]
+          expect(@dset.has_subset? dset2).to be false
+        end
+      end
+
+      describe "#superset?" do
+        it "returns true when all elements in the set are in the super set" do
+          dset2 = DSet.new ["a", "b", "c", "d", "e"]
+          expect(@dset.in_superset? dset2).to be true
+        end
+
+        it "returns true when all elements in the set are in the super set" do
+          expect(@dset.in_superset? [ "a", "b", "c", "d", "e"]).to be true
+        end
+
+        it "returns false if any elements from the set are missing from the superset" do
+          dset2 = DSet.new [ "b", "c", "d", "e"]
+          expect(@dset.in_superset? dset2).to be false
+        end
+
+        it "returns false if any elements from the set are missing from the superset" do
+          expect(@dset.in_superset? [ "b", "c", "d", "e"]).to be false
+        end
+      end
+    end
   end
 end
